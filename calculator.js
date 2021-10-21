aeebdfc2f4dfad80c3eb646e9360a1d27708f8ea
Alex Mitchell <alexmitchelldev@protonmail.com>
2021-10-21 07:30:59 -0500

    add basic calculator functions

---
+++ calculator.js
@@ -0,0 +1,15 @@
+const divide = (a, b) => {
+    return a / b;
+};
+
+const multiply = (a, b) => {
+    return a * b;
+};
+
+const subtract = (a, b) => {
+    return a - b;
+};
+
+const add = (a, b) => {
+    return a + b;
+};
